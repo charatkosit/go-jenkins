814196a26f91fe1f15f8f5962e15ce09c5c82171
charat <charatkosit@gmail.com>
2022-11-19 09:47:17 +0700

    release 0.8.4

---
+++ all-script/update-script-back-prod.sh
@@ -0,0 +1,25 @@
+!#/bin/bash
+git clone https://github.com/charatkosit/go-jenkins.git temp 
+
+#remove 
+rm -rf docker-mysql-pma
+rm -rf script
+
+#make dir
+mkdir script
+mkdir docker-mysql-pma
+
+#copy new
+cp /home/ec2-user/temp/all-script/aws-back-prod/script/*.* /home/ec2-user/script/
+cp /home/ec2-user/temp/all-script/aws-back-prod/docker-mysql-pma/*.* /home/ec2-user/docker-mysql-pma/
+#chmod
+chmod +x /home/ec2-user/script/*.*
+chmod +x /home/ec2-user/docker-mysql-pma/*.*
+#remove temp
+rm -rf temp
+
+# ls
+ls -l
+ls -l script
+ls -l docker-mysql-pma
+
---
+++ all-script/update-script-front-uat.sh
@@ -0,0 +1,21 @@
+!#/bin/bash
+git clone https://github.com/charatkosit/go-jenkins.git temp 
+
+#remove 
+rm -rf sscript
+
+#make dir
+mkdir script
+
+#copy new
+cp /home/ec2-user/temp/all-script/aws-back-uat/script/*.* /home/ec2-user/script/
+
+#chmod
+chmod +x /home/ec2-user/script/*.*
+
+#remove temp
+rm -rf temp
+
+# ls
+ls -l
+ls -l script
